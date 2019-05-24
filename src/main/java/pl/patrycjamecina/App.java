9b336af50335e4bd35f8321259385d7eff278dd7
Patrycja <mecina.patrycja@gmail.com>
2019-05-24 14:58:06 +0200

    init

---
+++ src/main/java/pl/patrycjamecina/App.java
@@ -0,0 +1,13 @@
+package pl.patrycjamecina;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
